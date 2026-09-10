8ab072321e122ba9e7ae547f9ed67e7cd51e5da6
Dagmar87 <jdfssobrinho@hotmail.com>
2026-09-10 11:01:56 -0300

    Criação do modelo de combustível

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class FuelType(models.TextChoices):
+  GASOLINE = "gasoline", "Gasolina"
+  ETHANOL = "ethanol", "Etanol"
+  DIESEL = "diesel", "Diesel"
+  FLEX = "flex", "Flex"
+
+class Vehicle(models.Model):
+  name = models.CharField(max_length=150)
+  
+  fuel_type = models.CharField(
+        max_length=20,
+        choices=FuelType.choices,
+    )
+  
+  consumption_km_per_liter = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+  
+  active = models.BooleanField(default=True)
+  
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+        return self.name
