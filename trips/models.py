e4bf37d809b59cd56a66c39af1a859ef3725ae79
Dagmar87 <jdfssobrinho@hotmail.com>
2026-09-10 11:09:10 -0300

    Criação do modelo da simulação

--- trips/models.py
+++ trips/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 class FuelType(models.TextChoices):
+  
   GASOLINE = "gasoline", "Gasolina"
   ETHANOL = "ethanol", "Etanol"
   DIESEL = "diesel", "Diesel"
   FLEX = "flex", "Flex"
 
 class Vehicle(models.Model):
+  
   name = models.CharField(max_length=150)
   
   fuel_type = models.CharField(
@@ -27,3 +29,103 @@ class Vehicle(models.Model):
   
   def __str__(self):
         return self.name
+      
+class TripSimulation(models.Model):
+  
+  origin = models.CharField(max_length=255)
+  
+  destination = models.CharField(max_length=255)
+  
+  origin_latitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        null=True,
+        blank=True,
+    )
+  
+  origin_longitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        null=True,
+        blank=True,
+    )
+  
+  destination_latitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        null=True,
+        blank=True,
+    )
+  
+  destination_longitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        null=True,
+        blank=True,
+    )
+  
+  fuel_price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+  
+  average_speed_kmh = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+  
+  round_trip = models.BooleanField(default=False)
+  
+  distance_km = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+  
+  duration_minutes = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+  
+  fuel_liters = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+  
+  fuel_cost = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+  
+  toll_cost = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0,
+    )
+  
+  total_cost = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+  
+  route_geometry = models.JSONField(
+        null=True,
+        blank=True,
+    )
+  
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+        return f"{self.origin} → {self.destination}"
+      
+
+
